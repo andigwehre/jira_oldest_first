ca99b96e5af64859434689f1f19b20bb2eaac6dc
andigwehre <git@agrill.at>
2024-01-18 15:04:35 +0100

    chrome poc finished

--- chrome/options.js
+++ chrome/options.js
@@ -1,13 +1,33 @@
+let Rule =   {
+    "id": 1,
+    "condition": {
+      "regexFilter": "^https://([^?]+)$",
+      "requestDomains": ["google.at"],
+      "resourceTypes": ["main_frame"]
+    },
+    "action": {
+      "type": "redirect",
+      "redirect": {
+        "regexSubstitution": "https://\\1?actionOrder=asc"
+      }
+    }
+  }
+chrome.declarativeNetRequest.updateDynamicRules({removeRuleIds: [1], addRules: [Rule]})
+
 function saveOptions(e) {
   e.preventDefault();
   chrome.storage.sync.set({
     urlFilter: document.querySelector("#urlFilter").value,
   });
+  console.log("saved "+document.querySelector("#urlFilter").value);
+  Rule.condition.requestDomains=[document.querySelector("#urlFilter").value];
+  chrome.declarativeNetRequest.updateDynamicRules({removeRuleIds: [1], addRules: [Rule]});
 }
 
 function restoreOptions() {
   function setCurrentChoice(result) {
     document.querySelector("#urlFilter").value = result.urlFilter || "";
+	console.log("restored "+ result.urlFilter);
   }
 
   function onError(error) {
